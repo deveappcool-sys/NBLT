535349fcb85ec278a051401e6003a7d3e111885a
NBLT <309387776+deveappcool-sys@users.noreply.github.com>
2026-07-26 16:54:31 -0400

    fix(dynamic): restore focus navigation to UP filter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.nblt.tv"
         minSdk = 23
         targetSdk = 35
-        versionCode = 44
-        versionName = "1.0.0"
+        versionCode = 45
+        versionName = "1.0.1"
     }
 
 
